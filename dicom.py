2b7d6583fef97172733d3d000ff13671764363da
Fuad Maulana <fuad.maulana@nawatech.co>
2024-07-10 12:58:27 +0700

    upload project

---
+++ dicom.py
@@ -0,0 +1,54 @@
+import numpy as np # linear algebra
+import SimpleITK as sitk
+
+def read_series(foldername) :
+    try :
+        print("Run read_series()")
+        # Read the DICOM series
+        series_reader = sitk.ImageSeriesReader()
+        dicom_names = series_reader.GetGDCMSeriesFileNames(foldername)
+        series_reader.SetFileNames(dicom_names)
+
+        # Load the series and convert to array
+        series = series_reader.Execute()
+        series_array = sitk.GetArrayFromImage(series)
+        return series_array
+    except Exception as e :
+        print("read_series() function get error :", e)
+
+def downsampling(series_array, factor) :
+    try :
+        print("Run downsampling()")
+        # For performance reasons, you might want to downsample your volume
+        # if the full resolution is not required for the visualization
+        series_downsampled = series_array[::factor, ::factor, ::factor]
+        return series_downsampled
+    except Exception as e :
+        print("downsampling() function get error :", e)
+
+def change_angle(series_downsampled, style) :
+    try :
+        print("Run change_angle()")
+        if style == 0 :
+            series_downsampled = np.transpose(series_downsampled, (0,1,2))
+        elif style == 1 :
+            series_downsampled = np.transpose(series_downsampled, (0,2,1))
+        elif style == 2 :
+            series_downsampled = np.transpose(series_downsampled, (1,0,2))
+        elif style == 3 :
+            series_downsampled = np.transpose(series_downsampled, (1,2,0))
+        elif style == 4 :
+            series_downsampled = np.transpose(series_downsampled, (2,0,1))
+        elif style == 5 :
+            series_downsampled = np.transpose(series_downsampled, (2,1,0))
+        return series_downsampled
+    except Exception as e :
+        print("change_angle() function get error :", e)
+
+def get_axis_series(series_downsampled, threshold) :
+    try :
+        print("Run get_axis_series()")
+        x, y, z = np.where(series_downsampled > threshold)
+        return x, y, z
+    except Exception as e :
+        print("get_axis_series() function get error :", e)
---
+++ main.py
@@ -0,0 +1,105 @@
+from dicom import *
+from fastapi import FastAPI, HTTPException, status, Security
+from fastapi.security import APIKeyHeader
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+foldername_ddr = "mount\dicom_ddr"
+foldername_lung = "mount\dicom_lung"
+foldername_mri = "mount\dicom_mri"
+API_KEYS = "0PfSuTqMuPrKPrJLs9IblYvzslI4u9GsgcayLVSaD4reu79gq1DFv1rRuw7GXqF2i5rsWS25oGJcmS7tJjPk39qSDA2NEcesVmkktPMH0cj5Y9Pl22gb3IFXyrAlxcgn"
+
+app = FastAPI()
+
+api_key = APIKeyHeader(name="api-key", auto_error=False)
+
+def get_api_key(
+    api_key: str = Security(api_key),
+) -> str:
+    """Retrieve and validate an API key from the query parameters or HTTP header.
+
+    Args:
+        api_key_query: The API key passed as a query parameter.
+        api_key_header: The API key passed in the HTTP header.
+
+    Returns:
+        The validated API key.
+
+    Raises:
+        HTTPException: If the API key is invalid or missing.
+    """
+    if api_key == API_KEYS:
+        return api_key
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
+
+class Item(BaseModel):
+    downsampling_factor: int
+    angle_style: int
+    threshold: int
+    data : dict
+
+@app.get("/process_dicom/ddr/", response_model=Item)
+async def process_dicom(downsampling_factor: int, angle_style: int, threshold: int, api_key: str = Security(get_api_key)) :
+    try :
+        series_array = read_series(foldername_ddr)
+        series_downsampled = downsampling(series_array, downsampling_factor)
+        series_downsampled = change_angle(series_downsampled, angle_style)
+        x, y, z = get_axis_series(series_downsampled, threshold)
+        clean_dicom = {
+            "downsampling_factor" : downsampling_factor,
+            "angle_style" : angle_style,
+            "threshold" : threshold,
+            "data" : {
+                "x" : x.tolist(),
+                "y" : y.tolist(),
+                "z" : z.tolist()
+            }
+        }
+        return clean_dicom
+    except Exception as e :
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.get("/process_dicom/lung/", response_model=Item)
+async def process_dicom(downsampling_factor: int, angle_style: int, threshold: int, api_key: str = Security(get_api_key)) :
+    try :
+        series_array = read_series(foldername_lung)
+        series_downsampled = downsampling(series_array, downsampling_factor)
+        series_downsampled = change_angle(series_downsampled, angle_style)
+        x, y, z = get_axis_series(series_downsampled, threshold)
+        clean_dicom = {
+            "downsampling_factor" : downsampling_factor,
+            "angle_style" : angle_style,
+            "threshold" : threshold,
+            "data" : {
+                "x" : x.tolist(),
+                "y" : y.tolist(),
+                "z" : z.tolist()
+            }
+        }
+        return clean_dicom
+    except Exception as e :
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.get("/process_dicom/mri/", response_model=Item)
+async def process_dicom(downsampling_factor: int, angle_style: int, threshold: int, api_key: str = Security(get_api_key)) :
+    try :
+        series_array = read_series(foldername_mri)
+        series_downsampled = downsampling(series_array, downsampling_factor)
+        series_downsampled = change_angle(series_downsampled, angle_style)
+        x, y, z = get_axis_series(series_downsampled, threshold)
+        clean_dicom = {
+            "downsampling_factor" : downsampling_factor,
+            "angle_style" : angle_style,
+            "threshold" : threshold,
+            "data" : {
+                "x" : x.tolist(),
+                "y" : y.tolist(),
+                "z" : z.tolist()
+            }
+        }
+        return clean_dicom
+    except Exception as e :
+        raise HTTPException(status_code=400, detail=str(e))
