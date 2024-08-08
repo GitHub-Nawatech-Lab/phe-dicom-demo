d57b3381b0556dd3318219031802fb72da9a0a7d
Fuad Maulana <fuad.maulana@nawatech.co>
2024-08-08 13:12:36 +0700

    update dicom.py

--- dicom.py
+++ dicom.py
@@ -5,51 +5,51 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import tempfile
 import os
 
-account_name = "amldevelopment2198817292"
-account_key = "c8Uq0oz26wsfynCqhEXjnElvk2FlbkNAosg+DMk5Bbs4OWtle634Ur1r5OqJCoZhdv9VK1OE6VhZ+AStvWgA6A=="
-container_name = "phe-dicom-dataset"
+# account_name = "amldevelopment2198817292"
+# account_key = "c8Uq0oz26wsfynCqhEXjnElvk2FlbkNAosg+DMk5Bbs4OWtle634Ur1r5OqJCoZhdv9VK1OE6VhZ+AStvWgA6A=="
+# container_name = "phe-dicom-dataset"
 
-def read_series_from_azure(foldername) :
-    try :
-        print("Run read_series_from_azure()")
-        # Create a BlobServiceClient
-        blob_service_client = BlobServiceClient(
-            account_url=f"https://{account_name}.blob.core.windows.net/",
-            credential=account_key
-        )
+# def read_series_from_azure(foldername) :
+#     try :
+#         print("Run read_series_from_azure()")
+#         # Create a BlobServiceClient
+#         blob_service_client = BlobServiceClient(
+#             account_url=f"https://{account_name}.blob.core.windows.net/",
+#             credential=account_key
+#         )
 
-        # Create a ContainerClient
-        container_client = blob_service_client.get_container_client(container_name)
+#         # Create a ContainerClient
+#         container_client = blob_service_client.get_container_client(container_name)
 
-        # Create a temporary directory to store the downloaded files
-        with tempfile.TemporaryDirectory() as temp_dir:
-            # List blobs in the directory
-            blob_list = container_client.list_blobs(name_starts_with=foldername)
+#         # Create a temporary directory to store the downloaded files
+#         with tempfile.TemporaryDirectory() as temp_dir:
+#             # List blobs in the directory
+#             blob_list = container_client.list_blobs(name_starts_with=foldername)
 
-            # Download each blob to the temporary directory
-            for blob in blob_list:
-                blob_client = container_client.get_blob_client(blob)
-                download_file_path = os.path.join(temp_dir, os.path.basename(blob.name))
-                print(f"Downloading blob: {blob.name} to {download_file_path}")
-                with open(download_file_path, "wb") as download_file:
-                    download_file.write(blob_client.download_blob().readall())
-                print(f"Downloaded blob: {blob.name}")
+#             # Download each blob to the temporary directory
+#             for blob in blob_list:
+#                 blob_client = container_client.get_blob_client(blob)
+#                 download_file_path = os.path.join(temp_dir, os.path.basename(blob.name))
+#                 print(f"Downloading blob: {blob.name} to {download_file_path}")
+#                 with open(download_file_path, "wb") as download_file:
+#                     download_file.write(blob_client.download_blob().readall())
+#                 print(f"Downloaded blob: {blob.name}")
 
-            # Run the SimpleITK code with the local directory path
-            series_reader = sitk.ImageSeriesReader()
-            dicom_names = series_reader.GetGDCMSeriesFileNames(temp_dir)
-            series_reader.SetFileNames(dicom_names)
-            image = series_reader.Execute()
+#             # Run the SimpleITK code with the local directory path
+#             series_reader = sitk.ImageSeriesReader()
+#             dicom_names = series_reader.GetGDCMSeriesFileNames(temp_dir)
+#             series_reader.SetFileNames(dicom_names)
+#             image = series_reader.Execute()
 
-            # Process the image as needed
-            print(f"Image size: {image.GetSize()}")
+#             # Process the image as needed
+#             print(f"Image size: {image.GetSize()}")
 
-        # Load the series and convert to array
-        series = series_reader.Execute()
-        series_array = sitk.GetArrayFromImage(series)
-        return series_array
-    except Exception as e :
-        print("read_series_from_azure() function get error :", e)
+#         # Load the series and convert to array
+#         series = series_reader.Execute()
+#         series_array = sitk.GetArrayFromImage(series)
+#         return series_array
+#     except Exception as e :
+#         print("read_series_from_azure() function get error :", e)
 
 def read_series(foldername) :
     try :
@@ -103,6 +103,6 @@ def get_axis_series(series_downsampled, threshold) :
     except Exception as e :
         print("get_axis_series() function get error :", e)
 
-if __name__ == "__main__" :
-    read_series_from_azure("dicom_lung")
+# if __name__ == "__main__" :
+#     read_series_from_azure("dicom_lung")
     # read_series("./mount/dicom_lung")
